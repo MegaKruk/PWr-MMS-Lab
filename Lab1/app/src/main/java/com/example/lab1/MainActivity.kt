411db122db47dc00534c554a79cd6d95989f2aa6
Filip Mazur <226018@student.pwr.edu.pl>
2020-03-20 16:02:02 +0100

    Created simple BMI counting method. App crashes on emulator as soon as listener on button activates

--- Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.lab1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        outputBMI.text = inputMassKG.text.div((inputHeightCM.text.div(100.0)))
+//    }
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    buttonCount.setOnClickListener{
+        if (heightCM.text.isNotEmpty() && massKG.text.isNotEmpty())
+        {
+            val mass = massKG.text.toString().toDouble()
+            val height = heightCM.text.toString().toDouble()/100
+            val bmi = calculateBMI(mass, height)
+            outputBMI.text = bmi.toString()
+        }
     }
 }
+
+    private fun calculateBMI(weight: Double, height: Double) = BigDecimal(weight / (height * height))
+        .setScale(2, RoundingMode.HALF_EVEN).toDouble()
+}
+
