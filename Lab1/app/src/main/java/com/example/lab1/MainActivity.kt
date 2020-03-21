678419e617e2717077babeadd5ad9f092b44e8c9
Filip Mazur <226018@student.pwr.edu.pl>
2020-03-22 00:49:22 +0100

    Added BMI description and logic that checks input correctness

--- Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
@@ -21,17 +22,33 @@ class MainActivity : AppCompatActivity() {
         buttonEvent.setOnClickListener{
             val kg = inputMass.text.toString().toDouble()
             val cm = inputHeight.text.toString().toDouble()
+            if(kg < 0 || cm < 0 || kg > 300 || cm > 250)
+            {
+                outputInfo.text = "Wrong input!"
+            }
+            else
+            {
+                var bmi: Double = kg / Math.pow(cm / 100.0, 2.0)
+                bmi = (bmi * 100.0).roundToInt() / 100.0;
+                resultView.text = bmi.toString()
 
-            var bmi : Double = kg / Math.pow(cm / 100.0, 2.0)
-            bmi = (bmi * 100.0).roundToInt() / 100.0;
-            resultView.text = bmi.toString()
-
-//            when {
-//                bmi >= 30 -> helpView.text = "fat"
-//                bmi >= 25 -> helpView.text = "ok"
-//                bmi >= 13 -> helpView.text = "thin"
-//                else -> helpView.text = "wrong input"
-//            }
+                //            Underweight = 13-18.5
+                //            Normal weight = 18.5–24.9
+                //            Overweight = 25–29.9
+                //            Obesity = BMI of 30 or greater
+                when
+                {
+                    bmi >= 30 -> outputInfo.text =
+                        "You are obese. You should seek help of dietician."
+                    bmi >= 25 -> outputInfo.text =
+                        "You are overweight. You should eat less and exercise more."
+                    bmi >= 18.5 -> outputInfo.text =
+                        "You have normal weight. Keep doing what you're doing. Unless it's illegal. Then you should stop."
+                    bmi >= 13 -> outputInfo.text =
+                        "You are underweight. Use bigger plates and don't drink water before eating. Have some pizza."
+                    else -> outputInfo.text = "wrong input (or dead)"
+                }
+            }
 
         }
     }
