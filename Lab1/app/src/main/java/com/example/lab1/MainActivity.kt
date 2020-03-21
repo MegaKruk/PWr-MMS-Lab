6e7d588035879f8ec86d90554d757e2cadefe2cb
Filip Mazur <226018@student.pwr.edu.pl>
2020-03-21 17:18:29 +0100

    Used different approach. Finally no crashing. Still no idea what caused the crash commit ago

--- Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,48 +2,38 @@ package com.example.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//        outputBMI.text = inputMassKG.text.div((inputHeightCM.text.div(100.0)))
-//    }
-override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    buttonCount.setOnClickListener{
-        if (heightCM.text.isNotEmpty() && massKG.text.isNotEmpty())
-        {
-            val mass = massKG.text.toString()
-            val height = heightCM.text.toString()
-            val bmiResult = calculateBMI(mass, height)
-            outputBMI.text = bmiResult.toString()
-        } else
-        {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val inputHeight = findViewById<EditText>(R.id.inputHeightCM)
+        val inputMass = findViewById<EditText>(R.id.inputMassKG)
+        val buttonEvent = findViewById<Button>(R.id.buttonCount)
+        val resultView = findViewById<TextView>(R.id.outputBMI)
+
+        buttonEvent.setOnClickListener{
+            val kg = inputMass.text.toString().toDouble()
+            val cm = inputHeight.text.toString().toDouble()
+
+            var bmi : Double = kg / Math.pow(cm / 100.0, 2.0)
+            bmi = (bmi * 100.0).roundToInt() / 100.0;
+            resultView.text = bmi.toString()
+
+//            when {
+//                bmi >= 30 -> helpView.text = "fat"
+//                bmi >= 25 -> helpView.text = "ok"
+//                bmi >= 13 -> helpView.text = "thin"
+//                else -> helpView.text = "wrong input"
+//            }
 
         }
-}
-}
-
-
-
-//    private fun calculateBMI(weight: Double, height: Double) = BigDecimal(weight / (height * height))
-//        .setScale(2, RoundingMode.HALF_EVEN).toDouble()
-//}
-
-    private fun calculateBMI(height: String, weight: String): Double
-    {
-        val heightDouble = height.toDouble() / 100
-        val weightDouble = weight.toDouble()
-        //val bmiResult = String.format("%.2f", bmi)
-        return weightDouble / (heightDouble * heightDouble)
-        //val intent = Intent(this, ResultActivity::class.java)
-        //intent.putExtra("Calculate results here", bmiResult)
-        //startActivity(intent)
     }
 }
-
 
