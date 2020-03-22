51c2db22022c25b00b4648241f5b894fa7b80343
Filip Mazur <226018@student.pwr.edu.pl>
2020-03-22 02:20:27 +0100

    AboutActivity doesn't see activity_about.xml

--- Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.lab1
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.roundToInt
 
@@ -16,15 +21,22 @@ class MainActivity : AppCompatActivity() {
 
         val inputHeight = findViewById<EditText>(R.id.inputHeightCM)
         val inputMass = findViewById<EditText>(R.id.inputMassKG)
-        val buttonEvent = findViewById<Button>(R.id.buttonCount)
         val resultView = findViewById<TextView>(R.id.outputBMI)
 
+        val aboutEvent = findViewById<Button>(R.id.buttonAbout)
+        aboutEvent.setOnClickListener{
+            val intent = Intent(this, AboutActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonEvent = findViewById<Button>(R.id.buttonCount)
         buttonEvent.setOnClickListener{
             val kg = inputMass.text.toString().toDouble()
             val cm = inputHeight.text.toString().toDouble()
-            if(kg < 0 || cm < 0 || kg > 300 || cm > 250)
+            if(kg < 30 || cm < 100 || kg > 300 || cm > 250)
             {
                 outputInfo.text = "Wrong input!"
+                outputInfo.setTextColor(Color.BLACK)
             }
             else
             {
@@ -38,18 +50,33 @@ class MainActivity : AppCompatActivity() {
                 //            Obesity = BMI of 30 or greater
                 when
                 {
-                    bmi >= 30 -> outputInfo.text =
-                        "You are obese. You should seek help of dietician."
-                    bmi >= 25 -> outputInfo.text =
-                        "You are overweight. You should eat less and exercise more."
-                    bmi >= 18.5 -> outputInfo.text =
-                        "You have normal weight. Keep doing what you're doing. Unless it's illegal. Then you should stop."
-                    bmi >= 13 -> outputInfo.text =
-                        "You are underweight. Use bigger plates and don't drink water before eating. Have some pizza."
-                    else -> outputInfo.text = "wrong input (or dead)"
+                    bmi >= 30 -> {
+                        outputInfo.text = "You are obese. You should seek help of dietician."
+                        outputInfo.setTextColor(Color.RED)
+                        resultView.setTextColor(Color.RED)
+                    }
+                    bmi >= 25 -> {
+                        outputInfo.text = "You are overweight. You should eat less and exercise more."
+                        outputInfo.setTextColor(Color.YELLOW)
+                        resultView.setTextColor(Color.YELLOW)
+                    }
+                    bmi >= 18.5 -> {
+                        outputInfo.text = "You have normal weight. Keep doing what you're doing. Unless it's illegal. Then you should stop."
+                        outputInfo.setTextColor(Color.GREEN)
+                        resultView.setTextColor(Color.GREEN)
+                    }
+                    bmi >= 13 -> {
+                        outputInfo.text = "You are underweight. Use bigger plates and don't drink water before eating. Have some pizza."
+                        outputInfo.setTextColor(Color.BLUE)
+                        resultView.setTextColor(Color.BLUE)
+                    }
+                    else -> {
+                        outputInfo.text = "wrong input (or dead)"
+                        outputInfo.setTextColor(Color.BLACK)
+                        resultView.setTextColor(Color.BLACK)
+                    }
                 }
             }
-
         }
     }
 }
