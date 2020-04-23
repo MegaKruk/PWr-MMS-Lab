b3c147cdd1e554cdc45d8c7795a69d5375012d27
Filip Mazur <226018@student.pwr.edu.pl>
2020-04-23 16:58:42 +0200

    Created Task class and added drawables for Task icons

---
+++ Lab2/app/src/main/java/com/example/lab2_taskmanager/Icon.kt
@@ -0,0 +1,9 @@
+package com.example.lab2_taskmanager
+
+enum class Icon(val id : Int)
+{
+    todo(R.drawable.todo),
+    email(R.drawable.email),
+    meeting(R.drawable.meeting),
+    phone(R.drawable.phone)
+}
---
+++ Lab2/app/src/main/java/com/example/lab2_taskmanager/Task.kt
@@ -0,0 +1,8 @@
+package com.example.lab2_taskmanager
+
+import android.icu.util.Calendar
+
+class Task(val id : Int, val title : String, val description : String, val date : Calendar, val status : Boolean, val iconType : Icon)
+{
+
+}
